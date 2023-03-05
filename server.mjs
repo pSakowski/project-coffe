6b0348108f98d40808750413512d0d514440ac3e
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-05 17:39:18 +0100

    fix

--- server.mjs
+++ server.mjs
@@ -4,15 +4,21 @@ import jsonServer from 'json-server';
 const server = jsonServer.create();
 const router = jsonServer.router(path.join('app.json'));
 const middlewares = jsonServer.defaults({
-  static: 'dist',
   noCors: true
 });
+
+// Serve static content from the dist folder
+// eslint-disable-next-line no-undef
+server.use(jsonServer.static(path.join(__dirname, 'dist')));
+
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3131;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 export default server;
